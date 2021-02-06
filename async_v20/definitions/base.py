accdb3c9778648a9a0a0ce7181866197cd5d416e
Nuno Goncalves <nuno.alberto.soares.goncalves@gmail.com>
2021-02-06 16:26:55 +0000

    Make health checkl options #21

--- async_v20/definitions/base.py
+++ async_v20/definitions/base.py
@@ -44,21 +44,21 @@ class Metaclass(type):
             pass
 
         would have the attribute foo_instance.foo == Bar
-        """
+    """
 
     def __new__(mcs, name, bases, namespace, **kwargs):
-        jit = kwargs.pop('jit', True)
+        jit = kwargs.pop("jit", True)
 
         try:
-            arg_names = tuple(signature(namespace.get('__init__')).parameters)
+            arg_names = tuple(signature(namespace.get("__init__")).parameters)
         except TypeError:
             arg_names = ()
 
-        slots = arg_names + tuple(kwargs) + tuple(namespace.get('__slots__', ()))
+        slots = arg_names + tuple(kwargs) + tuple(namespace.get("__slots__", ()))
         if jit:
-            slots = slots + tuple(map(lambda x: '_' + x, arg_names))
+            slots = slots + tuple(map(lambda x: "_" + x, arg_names))
 
-        namespace['__slots__'] = slots
+        namespace["__slots__"] = slots
 
         class_obj = super().__new__(mcs, name, bases, namespace)
 
@@ -68,16 +68,21 @@ class Metaclass(type):
 
         # jit == True object attribute instantiation is deferred
         class_obj._instantiate = {
-            True: lambda self, name, typ, data:
-            object.__setattr__(self, '_' + name, partial(create_attribute, typ, data)),
-            False: lambda self, name, typ, data:
-            object.__setattr__(self, name, create_attribute(typ, data))}[jit]
+            True: lambda self, name, typ, data: object.__setattr__(
+                self, "_" + name, partial(create_attribute, typ, data)
+            ),
+            False: lambda self, name, typ, data: object.__setattr__(
+                self, name, create_attribute(typ, data)
+            ),
+        }[jit]
 
         class_obj._preset_values = kwargs
 
-        if not class_obj.__name__ == 'Model':
+        if not class_obj.__name__ == "Model":
             # Only add the argument parser to objects that derive from Model
-            class_obj.__init__ = arg_parse(class_obj.__init__, bound_signature.parameters, kwargs)
+            class_obj.__init__ = arg_parse(
+                class_obj.__init__, bound_signature.parameters, kwargs
+            )
             class_obj.__init__.__signature__ = unbound_signature
 
         # Create a pretty signature for documentation
@@ -90,9 +95,9 @@ class Metaclass(type):
 
 class Model(object, metaclass=Metaclass):
     # Make attribute assignment impossible
-    __slots__ = ('_fields', '_str')
+    __slots__ = ("_fields", "_str")
 
-    _delimiter = '_'
+    _delimiter = "_"
 
     def __setattr__(self, key, value):
         raise NotImplementedError
@@ -107,25 +112,34 @@ class Model(object, metaclass=Metaclass):
         def information():
 
             # These attributes seem to be the most important to users
-            for attribute in ('id', 'instrument', 'amount', 'units',
-                              'current_units', 'realized_pl',
-                              'unrealized_pl', 'price', 'reason', 'time'):
+            for attribute in (
+                "id",
+                "instrument",
+                "amount",
+                "units",
+                "current_units",
+                "realized_pl",
+                "unrealized_pl",
+                "price",
+                "reason",
+                "time",
+            ):
                 try:
                     value = getattr(self, attribute)
                 except (IndexError, AttributeError):
                     continue
                 if value is not None:
-                    yield f'{attribute}={value}'
+                    yield f"{attribute}={value}"
 
         # Attempt to get important attributes otherwise provide everything
-        attributes = ', '.join(information())
+        attributes = ", ".join(information())
         if not attributes:
-            attributes = ', '.join(self._fields)
+            attributes = ", ".join(self._fields)
 
-        return f'<{self.__class__.__name__}: {attributes}>'
+        return f"<{self.__class__.__name__}: {attributes}>"
 
     def __getattr__(self, item):
-        result = self.__getattribute__('_' + item)()
+        result = self.__getattribute__("_" + item)()
         object.__setattr__(self, item, result)
         return result
 
@@ -158,8 +172,8 @@ class Model(object, metaclass=Metaclass):
             else:
                 fields.append(name)
                 self._instantiate(name, annotation, value)
-        object.__setattr__(self, '_str', f'{self.__class__.__name__}(**{kwargs})')
-        object.__setattr__(self, '_fields', tuple(fields))
+        object.__setattr__(self, "_str", f"{self.__class__.__name__}(**{kwargs})")
+        object.__setattr__(self, "_fields", tuple(fields))
 
     def get(self, name, default=None):
         return getattr(self, name, default)
@@ -187,12 +201,15 @@ class Model(object, metaclass=Metaclass):
                         attr = attr.dict(json=json, datetime_format=datetime_format)
                     except AttributeError:
                         try:
-                            attr = [obj.dict(json=json, datetime_format=datetime_format) for obj in attr]
+                            attr = [
+                                obj.dict(json=json, datetime_format=datetime_format)
+                                for obj in attr
+                            ]
                         except AttributeError:
-                            attr = [str(obj)
-                                    if json and isinstance(obj, float)
-                                    else obj
-                                    for obj in attr]
+                            attr = [
+                                str(obj) if json and isinstance(obj, float) else obj
+                                for obj in attr
+                            ]
                         except TypeError:
                             # Attr is None. account_changes endpoint
                             # returns items with null
@@ -204,16 +221,18 @@ class Model(object, metaclass=Metaclass):
                     # to strings when sending JSON data to OANDA
                     attr = str(attr)
                 elif isinstance(attr, pd.Timestamp):
-                    if json or datetime_format == 'RFC3339':
+                    if json or datetime_format == "RFC3339":
                         attr = attr.json(datetime_format=datetime_format)
-                    elif datetime_format == 'UNIX':
+                    elif datetime_format == "UNIX":
                         attr = attr.value
 
                 yield field, attr
 
-        return {json_attributes[field] if json else field: attr for field, attr in fields()}
+        return {
+            json_attributes[field] if json else field: attr for field, attr in fields()
+        }
 
-    def json(self, datetime_format='UNIX'):
+    def json(self, datetime_format="UNIX"):
         """Return the JSON representation of the object
 
         Args:
@@ -233,7 +252,9 @@ class Model(object, metaclass=Metaclass):
         """
         if delimiter is None:
             delimiter = self._delimiter
-        return flatten_dict(self.dict(json=json, datetime_format=datetime_format), delimiter)
+        return flatten_dict(
+            self.dict(json=json, datetime_format=datetime_format), delimiter
+        )
 
     def series(self, json=False, datetime_format=None, delimiter=None):
         """Return a :class:`pandas.Series` representation of the object
@@ -245,7 +266,9 @@ class Model(object, metaclass=Metaclass):
               corresponding format
             delimiter: Value to use when flattening the the data structure. Defaults to `_`
         """
-        return pd.Series(self.data(json=json, datetime_format=datetime_format, delimiter=delimiter))
+        return pd.Series(
+            self.data(json=json, datetime_format=datetime_format, delimiter=delimiter)
+        )
 
 
 class Array(object):
@@ -280,8 +303,8 @@ class Array(object):
         handled by self.__getitem__(self, key).
 
         """
-        object.__setattr__(self, '_items', items)
-        object.__setattr__(self, 'items', [])
+        object.__setattr__(self, "_items", items)
+        object.__setattr__(self, "items", [])
 
     def __init_subclass__(cls, **kwargs):
         """Record the type *contained in* the subclass-array.
@@ -294,10 +317,10 @@ class Array(object):
         will have all its inner objects instantiated using class Foo.
 
         """
-        cls._contains = kwargs.pop('contains')
+        cls._contains = kwargs.pop("contains")
 
     def __repr__(self):
-        return f'<{self._contains.__name__} x {len(self)}>'
+        return f"<{self._contains.__name__} x {len(self)}>"
 
     def __hash__(self):
         return hash(str(self._items))
@@ -323,15 +346,16 @@ class Array(object):
 
     def __getitem__(self, key):
         if isinstance(key, slice):
-            return self.__class__(*[self._items[index]
-                                    for index in range(len(self._items))[key]])
+            return self.__class__(
+                *[self._items[index] for index in range(len(self._items))[key]]
+            )
 
         length = len(self._items)
         if key < 0:
             key += length
 
         if not (0 <= key < length):
-            raise IndexError('Array index out of range')
+            raise IndexError("Array index out of range")
 
         if key >= len(self.items):
             self.items.__iadd__([None] * (key - len(self.items) + 1))
@@ -370,16 +394,15 @@ class Array(object):
             # json_model is either a json_dict. Or a Model object
             # constructed from the json_dict
             try:
-                key = json_model.get('id', None)
+                key = json_model.get("id", None)
                 if key is not None:
                     id_index.update({str(key): index})
 
-
-                key = json_model.get('trade_id', json_model.get('tradeID'))
+                key = json_model.get("trade_id", json_model.get("tradeID"))
                 if key is not None:
                     trade_id_index.setdefault(str(key), []).append(index)
 
-                key = json_model.get('instrument', json_model.get('name', None))
+                key = json_model.get("instrument", json_model.get("name", None))
                 if key is not None:
                     instrument_index.setdefault(key, []).append(index)
             except AttributeError:
@@ -388,7 +411,7 @@ class Array(object):
         return dict(
             _id_index=id_index,
             _trade_id_index=trade_id_index,
-            _instrument_index=instrument_index
+            _instrument_index=instrument_index,
         )
 
     def get_id(self, id_, default=None):
@@ -415,7 +438,7 @@ class Array(object):
         try:
             if type is not None:
                 for obj in (self[idx] for idx in self._trade_id_index[str(id_)]):
-                    if getattr(obj, 'type', None) == type:
+                    if getattr(obj, "type", None) == type:
                         return obj
                 return default
 
@@ -429,11 +452,12 @@ class Array(object):
         `object.trade_id` attribute matches the passed id
         else return the default"""
         try:
-            return self.__class__(*[self[idx] for idx in self._trade_id_index[str(id_)]])
+            return self.__class__(
+                *[self[idx] for idx in self._trade_id_index[str(id_)]]
+            )
         except KeyError:
             pass
         return default
-
 
     def get_instrument(self, instrument, default=None):
         """Return the first object in the array where
@@ -450,7 +474,9 @@ class Array(object):
         `object.instrument` attribute matches the passed instrument
         else return the default"""
         try:
-            return self.__class__(*[self[idx] for idx in self._instrument_index[instrument]])
+            return self.__class__(
+                *[self[idx] for idx in self._instrument_index[instrument]]
+            )
         except KeyError:
             pass
         return default
@@ -464,7 +490,9 @@ class Array(object):
 
             datetime_format: 'UNIX' or 'RFC3339'
         """
-        return pd.DataFrame(obj.data(json=json, datetime_format=datetime_format) for obj in self)
+        return pd.DataFrame(
+            obj.data(json=json, datetime_format=datetime_format) for obj in self
+        )
 
 
 def create_attribute(typ, data):
@@ -474,7 +502,7 @@ def create_attribute(typ, data):
             result = typ(**data)
         elif isinstance(data, Specifier):
             if not issubclass(typ, Specifier):
-                msg = f'{data} must be a {Specifier} is {type(data)}'
+                msg = f"{data} must be a {Specifier} is {type(data)}"
                 logger.error(msg)
                 raise IncompatibleValue(msg)
             result = typ(data)
@@ -482,7 +510,7 @@ def create_attribute(typ, data):
             result = typ(data)
         elif isinstance(data, (Model, Array, Primitive)):
             if not issubclass(type(data), typ):
-                msg = f'{data} must be of type {typ} is {type(data)}'
+                msg = f"{data} must be of type {typ} is {type(data)}"
                 logger.error(msg)
                 raise IncompatibleValue(msg)
             result = data
@@ -496,7 +524,7 @@ def create_attribute(typ, data):
         # when an error code has been returned
         # A none value should be returned if this is the case
         if typ is not None:
-            msg = f'Could not create {typ}. DATA: {data}, TYPE: {type(data)}'
+            msg = f"Could not create {typ}. DATA: {data}, TYPE: {type(data)}"
             logger.error(msg)
             raise InstantiationFailure(msg)
     else:
